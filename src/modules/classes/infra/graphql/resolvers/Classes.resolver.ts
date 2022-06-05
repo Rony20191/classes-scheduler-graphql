4a60a0040b2e4d17483cccf088a1e35d1b5c08e8
Rony20191 <ronieresilva18@gmail.com>
2022-06-05 14:35:27 -0300

    feat: add query classes

--- src/modules/classes/infra/graphql/resolvers/Classes.resolver.ts
+++ src/modules/classes/infra/graphql/resolvers/Classes.resolver.ts
@@ -1,4 +1,4 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 
 import { CreateClassUseCase } from '~modules/classes/useCases/createClass/CreateClass.useCase';
 
@@ -14,6 +14,31 @@ class ClassesResolver {
     const classy = await this.createClassUseCase.execute(input);
 
     return classy;
+  }
+  @Query(() => [Class])
+  async classes(): Promise<Class[]> {
+    return [
+      {
+        id: '1',
+        title: 'Class 1',
+        description: 'Description 1',
+        link: 'Link 1',
+        minimum_level_id: '1',
+        teacher_id: '1',
+        created_at: new Date(),
+        updated_at: null,
+      },
+      {
+        id: '2',
+        title: 'Class 2',
+        description: 'Description 2',
+        link: 'Link 2',
+        minimum_level_id: '2',
+        teacher_id: '2',
+        created_at: new Date(),
+        updated_at: null,
+      },
+    ];
   }
 }
 
